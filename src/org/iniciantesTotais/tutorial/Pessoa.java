c4f1beecfe0b9b600832431d3733daba1c336b2b
EstefanoMoreira <estefanomoreira@hotmail.com>
2019-01-22 04:23:50 -0200

    new file:   .classpath 	new file:   .gitignore 	new file:   .project 	new file:   src/org/iniciantesTotais/tutorial/Pessoa.java

---
+++ src/org/iniciantesTotais/tutorial/Pessoa.java
@@ -0,0 +1,31 @@
+package org.iniciantes.tutorial;
+
+public class Pessoa {
+	// Atributos
+	private String nome; //nome da pessoa
+	private int maxLivros; //max livros emprestar
+	
+	//construtor
+	public Pessoa() {
+		nome = "Nome Desconhecido";
+		maxLivros = 3;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String qualquerNome) {
+		nome = qualquerNome;
+	}
+
+	public int getMaxLivros() {
+		return maxLivros;
+	}
+
+	public void setMaxLivros(int maxLivros) {
+		this.maxLivros = maxLivros;
+	}
+	
+	
+}
