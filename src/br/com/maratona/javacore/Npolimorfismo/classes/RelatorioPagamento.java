471d52a694f54a4dbbac04c2383d8dfd341d35b7
estefano <estefano@Dell-Linux>
2019-01-24 16:39:19 +0000

    as

--- src/br/com/maratona/javacore/Npolimorfismo/classes/RelatorioPagamento.java
+++ src/br/com/maratona/javacore/Npolimorfismo/classes/RelatorioPagamento.java
@@ -1,19 +1,19 @@
 package br.com.maratona.javacore.Npolimorfismo.classes;
 
 public class RelatorioPagamento {
-	public void relatorioPagamentoGerente(Gerente gerente) {
-		System.out.println("Gerando relatorio de pagamento para gerencia");
-		gerente.calcularPagamento();
-		System.out.println("Nome: " + gerente.getNome());
-		System.out.println("Salario: " + gerente.getSalario());
-		
-	}
-	public void relatorioPagamentoVendedor(Vendedor vendedor) {
-		System.out.println("Gerando relatorio de pagamento para vendedores");
-		vendedor.calcularPagamento();
-		System.out.println("Nome: " + vendedor.getNome());
-		System.out.println("Salario: " + vendedor.getSalario());		
-	}
+//	public void relatorioPagamentoGerente(Gerente gerente) {
+//		System.out.println("Gerando relatorio de pagamento para gerencia");
+//		gerente.calcularPagamento();
+//		System.out.println("Nome: " + gerente.getNome());
+//		System.out.println("Salario: " + gerente.getSalario());
+//		
+//	}
+//	public void relatorioPagamentoVendedor(Vendedor vendedor) {
+//		System.out.println("Gerando relatorio de pagamento para vendedores");
+//		vendedor.calcularPagamento();
+//		System.out.println("Nome: " + vendedor.getNome());
+//		System.out.println("Salario: " + vendedor.getSalario());		
+//	}
 
 	public void relatorioPagamentoGenerico(Funcionario funcionario) {
 		System.out.println("Gerando relatorio de pagamento");
