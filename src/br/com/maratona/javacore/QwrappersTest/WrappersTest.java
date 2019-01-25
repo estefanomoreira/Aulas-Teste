198daa26253ea5c444057bf31af0a6b8df59b936
estefano <estefano@Dell-Linux>
2019-01-25 01:38:35 +0000

    wrapper

---
+++ src/br/com/maratona/javacore/QwrappersTest/WrappersTest.java
@@ -0,0 +1,29 @@
+package br.com.maratona.javacore.QwrappersTest;
+
+public class WrappersTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bytePrimitivo = 127;
+		short shortPrimitivo = -32768;
+		int inrPrimitivo = 2100000000;
+		long longPrimitivo = 10L;
+		float floatPrimitivo = 10F;
+		double doublePrimitivo = 10D;
+		char charPrimitivo = 'A';
+		boolean booleannPrimitivo = true;
+		
+		//wrapper de tipos primitivos:
+		Byte byteWrapper = 1;
+		Short shortWrapper = 1;
+		Integer integerWrapper = 1;
+		Long longWrapper =10L;
+		Float floatWrapper = 10F;
+		Double doubleWrapper = 10D;
+		Character characterWrapper = 'A';
+		Boolean booleanWrapper = true;
+		
+		
+	}
+
+}
